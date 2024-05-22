e63e815e6e45f0eae273310409a210e748e6dd14
teotoivo <teo.maximilien@gmail.com>
2024-05-22 12:38:15 +0300

    fixed a bug for running the wrong file

--- src/main.c
+++ src/main.c
@@ -58,7 +58,6 @@ int main()
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 60.0f;
     camera.projection = CAMERA_PERSPECTIVE;
-
     //--------------------------------------------------------------------------------------
 
 #if defined(PLATFORM_WEB)
